85e5de6fa2f8c3e9a049aa15931e319e0ad0fa5b
TCG0PX <codemaster628@gmail.com>
2026-08-26 11:29:04 +0530

    Implement random movement and jump for bot
    Added random movement and occasional jumping for the bot.

--- index.js
+++ index.js
@@ -13,6 +13,27 @@ function startBot() {
 
   bot.once('spawn', () => {
     console.log('SUCCESS: MoonLightBot joined the server!');
+
+    setInterval(() => {
+      // Pehle sab movement band
+      bot.clearControlStates();
+
+      // Random direction
+      const directions = ['forward', 'back', 'left', 'right'];
+      const direction =
+        directions[Math.floor(Math.random() * directions.length)];
+
+      bot.setControlState(direction, true);
+
+      // Kabhi-kabhi jump
+      if (Math.random() > 0.5) {
+        bot.setControlState('jump', true);
+
+        setTimeout(() => {
+          bot.setControlState('jump', false);
+        }, 500);
+      }
+    }, 3000);
   });
 
   bot.on('kicked', (reason) => {
